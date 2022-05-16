e2706e13a097763c2a81bcb3b41eff0560789c0d
Pranav Gautam <prnvgautam15@gmail.com>
2022-05-16 19:29:34 +0530

    Navigation is added and styled. Used React routes and router

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop">Shop</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
